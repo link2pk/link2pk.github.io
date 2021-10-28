c9356873e2fee5f8909d057a15438424deb3b726
Praveen Kumar <praveenkumar@Praveens-MacBook-Pro.local>
2021-10-28 19:42:02 +0530

    graphic designer profile updated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function(){
 	var load_screen = document.getElementById("load_screen");
-	document.body.removeChild(load_screen);
+	document.body.removeChild(load_screen)
 });
 
 
